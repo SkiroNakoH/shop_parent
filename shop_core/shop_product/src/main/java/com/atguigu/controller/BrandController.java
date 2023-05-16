e00a1a6f65b11b6d54cce785b3c0babda0846b66
SkiroNako <2681678536@qq.com>
2023-05-16 18:17:44 +0800

    shop_product fastdfs进行文件上传

--- shop_core/shop_product/src/main/java/com/atguigu/controller/BrandController.java
+++ shop_core/shop_product/src/main/java/com/atguigu/controller/BrandController.java
@@ -5,8 +5,14 @@ import com.atguigu.result.RetVal;
 import com.atguigu.service.BaseBrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.commons.io.FilenameUtils;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -61,4 +67,30 @@ public class BrandController {
         List<BaseBrand> brandList = brandService.list(null);
         return RetVal.ok(brandList);
     }
+
+    //7.文件上传
+    //7.http://127.0.0.1/product/brand/fileUpload
+    @PostMapping("/fileUpload")
+    public RetVal fileUpload1(MultipartFile file) throws Exception {
+        //需要一个配置文件告诉fastdfs在哪里
+        String configFilePath = this.getClass().getResource("/tracker.conf").getFile();
+        //初始化
+        ClientGlobal.init(configFilePath);
+        //创建trackerClient 客户端
+        TrackerClient trackerClient = new TrackerClient();
+        //用trackerClient获取连接
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //创建StorageClient1
+        StorageClient1 storageClient1 = new StorageClient1(trackerServer, null);
+        //对文件实现上传
+        String originalFilename = file.getOriginalFilename();
+        String extension = FilenameUtils.getExtension(originalFilename);
+        String path = storageClient1.upload_appender_file1(file.getBytes(), extension, null);
+
+//        System.out.println("trackerServer.getInetSocketAddress().getAddress() = " + trackerServer.getInetSocketAddress().getAddress().toString());
+        String filePath ="http:/" + trackerServer.getInetSocketAddress().getAddress().toString() + ":8888/" + path;
+        //拼接路径 http://192.168.121.128:8888/group1/M00/00/01/wKh5gGRiHAeEfdnWAAAAAIx0Xh4285.jpg
+        return RetVal.ok(filePath);
+    }
+
 }
