7d030c2bab2f15a647ee3eb79dbf8fda59c64933
SkiroNako <2681678536@qq.com>
2023-05-30 15:43:40 +0800

    shop_gateway 拦截用户非法访问内部接口

--- shop_gateway/src/main/java/com/atguigu/filter/AccessFilter.java
+++ shop_gateway/src/main/java/com/atguigu/filter/AccessFilter.java
@@ -1,23 +1,28 @@
 package com.atguigu.filter;
 
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.result.RetVal;
+import com.atguigu.result.RetValCodeEnum;
 import com.atguigu.util.IpUtil;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
@@ -26,6 +31,7 @@ public class AccessFilter implements GlobalFilter {
     private String filterWhiteFilter;
     @Autowired
     private RedisTemplate redisTemplate;
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     /**
      * @param exchange 交换机，与服务进行交互
@@ -37,6 +43,12 @@ public class AccessFilter implements GlobalFilter {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
 
+        //拦截用户非法请求内部接口
+        String path = request.getURI().getPath();
+        if (antPathMatcher.match("/sku/**", path)) {
+            return writerDataToBrowser(response, RetValCodeEnum.NO_PERMISSION);
+        }
+
         //判断用户是否登录过
         String userId = getUserId(request);
         //用户已登录，放行
@@ -46,7 +58,6 @@ public class AccessFilter implements GlobalFilter {
 
         String[] filterSplit = filterWhiteFilter.split(",");
         for (String filter : filterSplit) {
-            String path = request.getURI().getPath();
             if (path.indexOf(filter) != -1) {
                 //需要进行过滤
                 response.getHeaders().set(HttpHeaders.LOCATION, "http://passport.gmall.com/login.html?originalUrl=" + request.getURI());
@@ -58,6 +69,16 @@ public class AccessFilter implements GlobalFilter {
 
 //        放行
         return chain.filter(exchange);
+    }
+
+    private static Mono<Void> writerDataToBrowser(ServerHttpResponse response, RetValCodeEnum retValCodeEnum) {
+        //用户非法访问，返回警告
+        response.getHeaders().add("Content-Type", "application/json");
+        RetVal<Object> retVal = RetVal.build(null, retValCodeEnum);
+
+        byte[] bytes = JSONObject.toJSONString(retVal).getBytes(StandardCharsets.UTF_8);
+        DataBuffer dataBuffer = response.bufferFactory().wrap(bytes);
+        return response.writeWith(Mono.just(dataBuffer));
     }
 
     private String getUserId(ServerHttpRequest request) {
