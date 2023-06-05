9b0fa0830ccefb3dd7b40e6c605137d6c8d73338
SkiroNako <2681678536@qq.com>
2023-06-05 20:19:04 +0800

    shop_order 订单读写分离+分库分表

--- shop_core/shop_order/src/main/java/com/atguigu/controller/TOrderController.java
+++ shop_core/shop_order/src/main/java/com/atguigu/controller/TOrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.controller;
 
 import com.atguigu.entity.TOrder;
 import com.atguigu.entity.TOrderDetail;
+import com.atguigu.mapper.TOrderMapper;
 import com.atguigu.service.OrderInfoService;
 import com.atguigu.service.TOrderDetailService;
 import com.atguigu.service.TOrderService;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author SkiroNakoH
@@ -30,11 +32,13 @@ public class TOrderController {
     private TOrderService order1Service;
     @Autowired
     private TOrderDetailService detail1Service;
+    @Autowired
+    private TOrderMapper order1Mapper;
 
     //1.保存订单利用分库分表
     @GetMapping("test01/{loopNum}")
-    public void test01(@PathVariable Integer loopNum){
-        for (int i = 0; i <loopNum; i++) {
+    public void test01(@PathVariable Integer loopNum) {
+        for (int i = 0; i < loopNum; i++) {
             TOrder tOrder = new TOrder();
             tOrder.setOrderPrice(99);
             String uuid = UUID.randomUUID().toString();
@@ -42,11 +46,12 @@ public class TOrderController {
             tOrder.setOrderStatus("未支付");
             //设置分库分表字段
             int userId = new Random().nextInt(20);
-            tOrder.setUserId(Long.parseLong(userId+""));
-            System.out.println("用户id"+userId);
+            tOrder.setUserId(Long.parseLong(userId + ""));
+            System.out.println("用户id" + userId);
             order1Service.save(tOrder);
         }
     }
+
     //2.保存订单与订单详情分库分表
     @GetMapping("test02/{userId}")
     public void saveOrderInfo(@PathVariable Long userId) {
@@ -73,6 +78,17 @@ public class TOrderController {
         detail1Service.save(sanxin);
         System.out.println("执行完成");
     }
+
+
+    //3.查询订单与订单详情
+    @GetMapping("test03/{userId}")
+    public void test03(@PathVariable Long userId) {
+        List<TOrder> orderList = order1Mapper.queryOrderAndDetail(userId, null);
+        //根据订单的id查询订单(全库查询) 没有使用分片的键 速度相当慢
+//        List<TOrder> orderList=order1Mapper.queryOrderAndDetail(null,1665680576490893313L);
+        System.out.println(orderList);
+    }
+
 
 }
 
