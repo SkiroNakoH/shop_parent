49c47ec672722da75684bd08cb4f11e540638418
SkiroNako <2681678536@qq.com>
2023-06-12 14:00:08 +0800

    shop_quartz 定时任务

---
+++ shop_core/shop_quartz/src/main/java/com/atgugu/schedule/SeckillSchedule.java
@@ -0,0 +1,32 @@
+package com.atgugu.schedule;
+
+import com.atguigu.constant.MqConst;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@EnableScheduling
+@RestController
+public class SeckillSchedule {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    //每天6点执行
+    @Scheduled(cron = "0 0 6 * * ?")
+    public void sendMsg2ScanSeckill(){
+        //mq`通知
+        rabbitTemplate.convertAndSend(MqConst.SCAN_SECKILL_EXCHANGE,MqConst.SCAN_SECKILL_ROUTE_KEY,"");
+    }
+
+    //每天2点，清理秒杀信息
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void sendMsg2ClearRedis(){
+        //mq`通知
+        rabbitTemplate.convertAndSend(MqConst.CLEAR_REDIS_EXCHANGE,MqConst.CLEAR_REDIS_ROUTE_KEY,"");
+    }
+}
